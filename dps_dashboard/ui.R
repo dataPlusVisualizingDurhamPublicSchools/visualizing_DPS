4b514aa21fa3f5073c433db9c9ecaa87120a7726
Laurenwalker25 <lauren.walker@duke.edu>
2023-07-19 12:21:08 -0400

    home page translations

--- dps_dashboard/ui.R
+++ dps_dashboard/ui.R
@@ -209,8 +209,9 @@ body <- {dashboardBody(
                box(h3(strong(i18n$t("Visualizing Durham Public Schools"))),
                    width = 12,
                    background = "navy",
-                   htmlOutput("home_text"),
+                   htmlOutput("home_text1"),
                    )),
+             
              fluidRow(
                class = "text-center",
                box(
@@ -220,20 +221,18 @@ body <- {dashboardBody(
                  valueBox(17, i18n$t("Geospatial Variables"), icon = icon("map"), color = "light-blue", width = 4),
                  valueBox(41, i18n$t("School-Specific Variables"), icon = icon("pencil"), color = "light-blue", width = 4),
                  valueBox(1, i18n$t("Centralized Web Application"), icon = icon("window-restore"), color = "light-blue", width = 4))),
+             
              fluidRow(
                class = "text-center",
                box(title = strong(i18n$t("2 Universities, 50 Public Schools")),
                    solidHeader = TRUE,
                    width = 7,
-                   p(i18n$t("The Durham Public School District has 50 public schools: 27 elementary, 11 middle, 12 high.")),
-                   br(),
-                   p(i18n$t("Duke and NCCU both aim to foster equitable partnerships with Durham Public Schools. Prior Bass Connections research focused on understanding how to achieve this goal and found that one of the main barriers to meaningful engagement between Duke, NCCU, and Durham public schools is that “many university students lack an understanding of city and community dynamics.” Additionally, they found that there is a “lack of student volunteer training with Durham’s context, particularly in the areas of history, school-specific demographics, and implicit bias and power dynamics that may manifest in schools.”")),
-                   br(),
-                   p(strong(i18n$t("Motivated by this research, our project explores a way of visualizing schools as centers of the community that bring academics, health and social services, youth and community development, and community engagement together under one roof.")))),
+                   htmlOutput("home_text2")),
                box(width = 5,
                    background = "light-blue",
                    solidHeader = TRUE,
                    leafletOutput("home"))),
+             
              fluidRow(class = "text-center",
                       box(title = strong(i18n$t("View Our 50 Schools")),
                           width = 12,
